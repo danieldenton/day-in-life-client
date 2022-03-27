662738d3e048d210a22a462c795fc4369fa042f5
danieldenton <danielmdenton@gmail.com>
2022-03-27 11:23:00 -0700

    profile complete

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,51 +1,48 @@
-import Photo from '../partials/Photo'
-import { Link, useParams } from "react-router-dom"
+import Photo from "../partials/Photo";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default function Profile() {
-    const { id } = useParams()
-    const [photos, setPhotos] = useState([])
-    useEffect(() => {
-        (async () => {
-            try {
-                const token = localStorage.getItem("jwt")
-                const options = {
-                headers: {
-                "Authorization": token
-            }
-        }
-        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-vi/users/${id}`, options )
-        console.log(response.data)
-        setPhotos(response.data)
+  const { id } = useParams();
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = localStorage.getItem("jwt");
+        // console.log(token);
+        const options = {
+          headers: {
+            Authorization: token,
+          },
+        };
+        const response = await axios.get(
+          `${process.env.REACT_APP_SERVER_URL}/api-v1/users/${id}`,
+          options
+        );
+        console.log(response.data);
+        setPhotos(response.data.photos);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
 
-            } catch (err) {
-                console.log(err)
-            }
-        })()
-        
-        
-    }, [])
+  const userPhotos = photos.map((photo, idx) => {
+    const cloudImage = `https://res.cloudinary.com/dhs1wrqhp/image/upload/v1593119998/${photo.public_id}.png`;
 
-        // const userPhotos = photos.map((photo, idx) => {
-        //     return (
-        //       <div key={`photo-link${idx}`}>
-        //         <Link to={`pictures/${photos._id}`}></Link>
-        //         <div>
-        //         {photo.img}
-        //         </div>
-        //       </div>
-        // //     )
-        // })
     return (
-        <>
-            <h1>Testing</h1>
-            {/* {userPhotos} */}
-            <Photo />
-            <Photo />
-            <Photo />
-            <Photo />
-            <Photo />
-        </>
-    )
+      <div key={photo.public_id}>
+        <Link to={`pictures/${photo.public_id}`}>
+          <img src={cloudImage} key={photo.public_id} />;
+        </Link>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Testing</h1>
+      {userPhotos}
+    </div>
+  );
 }
